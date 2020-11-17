dffaec0be4b1104a9d4454dadc73648730cc35f6
irvinghu07 <rhu5@dons.usfca.edu>
2020-11-18 00:20:04 +0800

    compacting to new backend server

--- src/components/upload.vue
+++ src/components/upload.vue
@@ -1,29 +1,35 @@
 <template>
   <div>
-    <form id="uploadForm" action="upload" role="form" method="post" enctype=multipart/form-data>
+    <form id="uploadForm" role="form" method="post" enctype=multipart/form-data v-on:submit.prevent="upload">
       <input type="file" id="file" name="file">
       <input type=submit value=Upload>
     </form>
+    <p>{{ payload }}</p>
   </div>
 </template>
 
 <script>
 
+
 export default {
   name: "upload",
   data: function () {
-    return {}
+    return {
+      payload: 'placeholder'
+    }
   },
   methods: {
     async upload() {
       let imagefile = document.querySelector('#file');
-      let formData = new FormData().append(imagefile.files[0].value, imagefile.files[0]);
-      await this.$http.post('/upload', formData, {
+      // imagefile.files[0].value
+      let formData = new FormData()
+      formData.append("file", imagefile.files[0]);
+      await this.$http.post('/upload/single', formData, {
             headers: {
               'Content-Type': 'multipart/form-data',
             }
           }
-      ).then(res => res.data)
+      ).then(resp => console.log(resp.data))
     }
   }
 }
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   devServer: {
     proxy: {
-      '/upload': {
+      '/upload/single': {
         target: 'http://localhost:7777',
         changeOrigin: true
       },
